3213fd4bf923356c5b72ef337536a31394f4f1ff
Denis <carriere.denis@gmail.com>
2017-10-30 00:09:53 -0400

    Expose union

--- test.js
+++ test.js
@@ -3,6 +3,9 @@ const jsts = require('./')
 
 test('jsts-es', t => {
     t.assert(jsts.GeoJSONWriter)
+    t.assert(jsts.GeoJSONReader)
     t.assert(jsts.BoundaryOp)
+    t.assert(jsts.BufferOp)
+    t.assert(jsts.UnionOp)
     t.end()
 })
