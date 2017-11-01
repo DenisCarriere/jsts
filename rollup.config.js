a61fa8d4fba0bef323bcaaa8b2b0d654961912a0
Denis <carriere.denis@gmail.com>
2017-11-01 10:35:43 -0400

    Convert jsts to ES2015 compatible code

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+    input: 'index.js',
+    output: {
+        extend: true,
+        file: 'main.js',
+        format: 'cjs'
+    },
+    plugins: [
+        babel({
+            exclude: 'node_modules/**'
+        })
+    ]
+};
